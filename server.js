cbc8d624ef081db4a880715afd4ba91fe41aad22
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-01-16 11:11:39 -0800

    weather working

--- server.js
+++ server.js
@@ -11,101 +11,12 @@ app.use(cors());
 // const client = new pg.Client(process.env.DATABASE_URL);
 // client.on('error', err => console.error(err));
 
-let locations = {};
-
-
-/////////////////////////////////////ROUTES/////////////////////////
-
-// app.get('/location',locationHandler);
-app.get('/location',newlocationHandler);
-app.get('/weather', weatherHandler);
-app.use('*', nonFoundHandler); 
-app.use(errorHandler);
-
-///////////////////////////HANDLER FUNCTIONS////////////////////////////////
-// TYLER: consider removing unused code before merging with master :).
-// function locationHandler(request,response){
-//     // console.log(request.query.city);
-//   try{
-//     let city = request.query.city;
-//     const geoData = require('./data/geo.json');
-//     let geoDataResults = geoData[0];
-
-//     let locations = new MapObject(city, geoDataResults);
-  
-//     response.send(locations);
-//     response.status(200).json(locations);
-//   }
-//   catch (error) {
-//     errorHandler ('So sorry Location handler', request, response);
-//   }
-// }
-
-//TYLER: Vij! This is a redundant function. See how all of this code can live inside the route, itself? 
-//See how this function takes in the exact same arguments as the route? (the (request, response))
-
-function newlocationHandler (request,response){
-    let city = request.query.city;
-//     // let {search_query, formatted_query, latitude, longitude} = request.query;
-    let key = process.env.LOCATION_IQ_KEY;
-    let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
-    console.log("I got the newlocationHandler going");
-//     ///////need to caching location before making the url call
-    if (locations[url]) {
-      response.send(locations[url]);
-    }
-    else {
-      superagent.get(url)
-        .then(results => {
-          console.log("I got the newlocationHandler going");
-          console.log("these are results", results.body);
-          const geoData = results.body[0];
-        //TYLER:Building on previous comment, this new MapObject constructor call is where we can 'outsource' work to a function outside of the route. Its all set up!  
-          const mapObject = new MapObject(city, geoData);
-          locations[url] = mapObject;
-          response.send(mapObject);
-          response.status(200).json(mapObject);
-
-        })
-        .catch((error) => {
-          errorHandler ('So sorry Location handler here', request, response);
-        })
-    }
-}
-
-function weatherHandler(request,response){
-  // get data from darksky.json
-  try {
-    let weatherresponseArray = [];
-    console.log('the weather data is working');
-    const weatherData = require('./data/darksky.json');
-    let weatherArray = weatherData.daily.data;
-
-    weatherresponseArray = weatherArray.map(obj => new WeatherObject(obj));
-
-    console.log('this is my wweather response array', weatherresponseArray);
-
-    response.send(weatherresponseArray);
-    response.status(200).json(weatherresponseArray);
-  }
-  catch (error) {
-    errorHandler ('So sorry Weather handler', request, response);
-  }
-}
-
-//////////////////////////ERROR HANDLER //////////////////////////////////
-
-function errorHandler(error, request, response) { console.log('ERROR',error);
-  response.status(500).send(error);
-}
-
-function nonFoundHandler(request, response) {response.status(404).send('this route does not exist')
-};
+let cachedLocations = {};
 
 
 ////////////////////////// CONSTRUCTORS////////////////////////////////////
 
-//constructor function to get the information from geo.json file into the array
+//location constructor
 function MapObject (city, geoData) {
   this.search_query = city;
   this.formatted_query = geoData.display_name;
@@ -116,10 +27,76 @@ function MapObject (city, geoData) {
 /// weather constructor
 function WeatherObject (weather) {
   this.forecast = weather.summary;
-  let normaltimes = new Date(weather.time*1000).toString().slice(0,15);
-  this.time = normaltimes;
+  this.time = new Date(weather.time*1000).toString().slice(0,15);
 }
 
+/////////////////////////////////////ROUTES/////////////////////////
+
+app.get('/location',locationHandler);
+app.get('/weather', weatherHandler);
+app.use('*', nonFoundHandler); 
+app.use(errorHandler);
+
+///////////////////////////HANDLER FUNCTIONS//////////////////////////////
+
+function locationHandler (request,response){
+    let city = request.query.city;
+    let key = process.env.LOCATION_IQ_KEY;
+    let url = `https://us1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json&limit=1`;
+    
+///need to caching location using url before making the url call, it can also be done with object
+    if (cachedLocations[url]) {
+      response.send(cachedLocations[url]);
+    }
+    else {
+      superagent.get(url)
+        .then(locresults => {
+          console.log("I got the newlocationHandler going");
+          // console.log("these are results", locresults.body);
+          const geoData = locresults.body[0];
+          const mapObject = new MapObject(city, geoData);
+          // console.log("these are MapObject", mapObject);
+          cachedLocations[url] = mapObject;
+          // response.send(mapObject);
+          response.status(200).json(mapObject);
+
+        })
+        .catch(() => {
+          errorHandler ('So sorry Location handler here', request, response);
+        })
+    }
+}
+
+function weatherHandler(request,response){
+  // get data from darksky.json
+    console.log('the weather data is working');
+    let latitude = request.query.latitude;
+    let longitude = request.query.longitude;
+    let key = process.env.DARKSKY_API_KEY;
+    let url = `https://api.darksky.net/forecast/${key}/${latitude},${longitude}`;
+    console.log('url works fine here', url);
+
+    superagent.get(url)
+      .then(weatherobj =>  {
+        console.log('what is breakkng this?');
+        console.log('this is my wwwweather response array', weatherobj.body.daily.data);
+        const weatherresponseArray = weatherobj.body.daily.data.map(obj => new WeatherObject(obj));
+        // response.send(weatherresponseArray);
+        response.status(200).json(weatherresponseArray);
+      })
+      .catch((error)  => { 
+        errorHandler ('So sorry Weather handler not working', request, response)
+      });
+}
+
+//////////////////////////ERROR HANDLER //////////////////////////////////
+
+function errorHandler(error, request, response) { console.log('ERROR',error);
+  response.status(500).send(error);
+}
+
+function nonFoundHandler(request, response) {response.status(404).send('this route does not exist')
+};
 
 
 ////// SEQUEL APP.GET //////////////////////
