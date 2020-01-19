d04f10344568aba9892851f64f1101ceba6790a3
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-01-19 14:29:13 -0800

    completed refactoring the handlers

--- server.js
+++ server.js
@@ -1,21 +1,16 @@
 'use strict';
-// require the libraries
+///////////////////////////LIBRARIES /////////////////////////////////////
+
 const express = require('express'); 
 const app = express();
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
-const superagent = require('superagent');
 const cors = require('cors');
 app.use(cors());  
 const client = require('./lib/global/client');
 
-///////////////////////////LIBRARIES /////////////////////////////////////
 //global
-const constructNewArray = require('./lib/global/constructorArray function');
 const errorHandler = require('./lib/global/errorhandler');
-
-//constructors
-const Trails = require('./lib/trails/trails_Trails_constructor');
 
 //handlers
 
@@ -29,6 +24,8 @@ const moviesHandler = require('./lib/movies/movies_moviesHandler');
 
 const yelpHandler = require('./lib/yelp/yelp_yelpHandler');
 
+const trailHandler = require('./lib/trails/trails_trailHandler');
+
 /////////////////////////////////////ROUTES/////////////////////////
 app.get('/location',locationHandler);
 app.get('/weather', weatherHandler);
@@ -40,27 +37,10 @@ app.get('/trails', trailHandler);
 app.use('*', nonFoundHandler);
 app.use(errorHandler);
 
-///////////////////////////HANDLER FUNCTIONS//////////////////////////////
-
-
-function trailHandler (request, response) {
-  console.log('trying the trail handler');
-  let lat = request.query.latitude;
-  let lon = request.query.longitude;
-  let trailapi_key = process.env.TRAIL_API_KEY;
-  let trail_url = `https://www.hikingproject.com/data/get-trails?lat=${lat}&lon=${lon}&maxDistance=10&key=${trailapi_key}`;
-  superagent.get(trail_url)
-  .then(trailresults => {
-    let trailResultsArray = trailresults.body.trails;
-    const trailsArray = constructNewArray (trailResultsArray, Trails);
-    response.status(200).json(trailsArray);
-  })
-  .catch(() => errorHandler('So sorry, the trail handler did not work', request, response));
-}
 
 //////////////////////////ERROR HANDLER //////////////////////////////////
 
-function nonFoundHandler(request, response) {response.status(404).send('this route does not exist')
+function nonFoundHandler() {response.status(404).send('this route does not exist')
 };
 
 //////////// Connect to DB and Start the Web Server
