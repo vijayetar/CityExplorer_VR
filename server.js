f18a1f3fb7b509c2124f3b5458980d5131215333
Vijayeta Rangarajan <vijayetar@gmail.com>
2020-01-19 13:45:16 -0800

    refactored location Handler

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const constructNewArray = require('./lib/global/constructorArray function');
 const errorHandler = require('./lib/global/errorhandler');
 
 //constructors
+// const MapObject = require('./lib/locations/location_MapObject_constructor');
 const WeatherObject = require('./lib/weather/weather_WeatherObject_constructor');
 const Event = require('./lib/events/events_Event_constructor');
 const MoviesInfo = require('./lib/movies/movies_MoviesInfo_constructor');
@@ -22,7 +23,8 @@ const YelpReviews = require('./lib/yelp/yelp_YelpReviews_constructor');
 const Trails = require('./lib/trails/trails_Trails_constructor');
 
 //handlers
-const locationHandler = require('./lib/locations/locations_locationHandler');
+
+const locationHandler = require('./lib/locations/location_handler');
 
 /////////////////////////////////////ROUTES/////////////////////////
 app.get('/location',locationHandler);
@@ -36,6 +38,45 @@ app.use('*', nonFoundHandler);
 app.use(errorHandler);
 
 ///////////////////////////HANDLER FUNCTIONS//////////////////////////////
+
+// function locationHandler (request,response){
+//   let city = request.query.city;
+//   let sql = 'SELECT * FROM city_explorer WHERE search_query = $1;';
+//   let safeValues = [city];
+//   client.query (sql, safeValues)
+//     .then (results  => {
+//       // console.log ('this is the city:',results,'this is rows', results.rows);
+//       if (results.rows.length>0){
+//         console.log('found results in db');
+//         response.send(results.rows[0]);
+//         return;
+//       }
+//       else {
+//         console.log('could not find in the db going to the api');
+//         let locationIQ_key = process.env.LOCATION_IQ_KEY;
+//         let locationIQ_url = `https://us1.locationiq.com/v1/search.php?key=${locationIQ_key}&q=${city}&format=json&limit=1`;
+
+//         superagent.get (locationIQ_url)
+//         .then(locresults => {
+//           console.log("I got the newlocationHandler going");
+//           const geoData = locresults.body[0];
+//           console.log('this is the geoData', geoData);
+//           const mapObject = new MapObject(city, geoData);
+//           console.log('this is ')
+//           let sql2 = `INSERT INTO city_explorer (search_query, formatted_query, latitude, longitude) VALUES ($1, $2, $3, $4);`;
+//           let safeValues = [mapObject.search_query, mapObject.formatted_query, mapObject.latitude, mapObject.longitude];
+//           client.query(sql2, safeValues);
+//           response.status(200).json(mapObject);
+//         })
+//         .catch(() => {
+//           errorHandler ('So sorry deeper Location handler here', request, response);
+//         })
+//       }
+//     })
+//     .catch(() => {
+//       errorHandler ('So sorry outside Location handler here', request, response);
+//     })
+// }
 
 function weatherHandler(request,response){
   // get data from darksky.json
@@ -93,7 +134,6 @@ function yelpHandler (request, response) {
 
   superagent.get(yelp_url)
   .set({'Authorization': `Bearer ${yelpapi_key}`})
-  // yelpclient.search(searchRequest)
   .then(yelpresults => {
     let yelpparsedresults = JSON.parse(yelpresults.text);
     let yelpparsedresultArray = yelpparsedresults.businesses;
@@ -121,11 +161,8 @@ function trailHandler (request, response) {
 
 //////////////////////////ERROR HANDLER //////////////////////////////////
 
-
-
 function nonFoundHandler(request, response) {response.status(404).send('this route does not exist')
 };
-
 
 //////////// Connect to DB and Start the Web Server
 client.connect()
